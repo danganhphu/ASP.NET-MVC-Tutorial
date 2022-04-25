9d7d397f327310d8a1c25fdc337f044df4ee7f15
danganhphu <phudanganh107@gmail.com>
2022-04-25 07:41:56 +0700

    update QLNV

---
+++ DangAnhPhu_61134177/QLNV_61134177/Controllers/NhanVien_61134177Controller.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLNV_61134177.Models;
+
+namespace QLNV_61134177.Controllers
+{
+    public class NhanVien_61134177Controller : Controller
+    {
+        private QLNV_61134177Entities db = new QLNV_61134177Entities();
+
+        // GET: NhanVien_61134177
+        public ActionResult Index()
+        {
+            var nhanViens = db.NhanViens.Include(n => n.PhongBan);
+            return View(nhanViens.ToList());
+        }
+
+        // GET: NhanVien_61134177/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            if (nhanVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanVien);
+        }
+
+        // GET: NhanVien_61134177/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaPB = new SelectList(db.PhongBans, "MaPB", "TenPB");
+            return View();
+        }
+
+        // POST: NhanVien_61134177/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaNV,HoNV,TenNV,GioiTinh,NgaySinh,Luong,AnhNV,DiaChi,MaPB")] NhanVien nhanVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NhanViens.Add(nhanVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaPB = new SelectList(db.PhongBans, "MaPB", "TenPB", nhanVien.MaPB);
+            return View(nhanVien);
+        }
+
+        // GET: NhanVien_61134177/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            if (nhanVien == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaPB = new SelectList(db.PhongBans, "MaPB", "TenPB", nhanVien.MaPB);
+            return View(nhanVien);
+        }
+
+        // POST: NhanVien_61134177/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNV,HoNV,TenNV,GioiTinh,NgaySinh,Luong,AnhNV,DiaChi,MaPB")] NhanVien nhanVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nhanVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaPB = new SelectList(db.PhongBans, "MaPB", "TenPB", nhanVien.MaPB);
+            return View(nhanVien);
+        }
+
+        // GET: NhanVien_61134177/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            if (nhanVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanVien);
+        }
+
+        // POST: NhanVien_61134177/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            db.NhanViens.Remove(nhanVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
